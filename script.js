cc26c74bc5c25a09c1c54a8e168eb900e994e9e3
Reginald921 <70420256+Reginald921@users.noreply.github.com>
2020-10-26 17:20:26 -0500

    Added game logic for result

---
+++ script.js
@@ -0,0 +1,52 @@
+let randomBall = (min, max) =>  {
+  return Math.floor(Math.random() * (max - min) + min);
+    };
+    let ballValue = (randomBall(1, 4));
+ console.log(ballValue);
+
+let clicked_id = 9;
+let decision = 0;
+let result_txt = "blank";
+  chosenBtn = 0;
+  console.log(chosenBtn);
+
+BtnClick();
+function BtnClick(clicked_id) {
+     chosenBtn = clicked_id;
+     console.log(chosenBtn + "cup")
+     console.log(ballValue + "ball");
+    verdict();
+}
+
+function verdict() {
+  if(ballValue == 0 || chosenBtn == 0 || chosenBtn == undefined) {
+    decision = "blank";
+    console.log(decision);
+    result_log();
+   } else if (ballValue == chosenBtn) {
+    decision = "win";
+    console.log(decision);
+    result_log();
+   } else if (ballValue !== chosenBtn)
+    decision = "lost";
+   console.log(decision);
+   result_log();
+   }
+   
+
+   function result_log() {
+    if(decision == "win") {
+      result_txt = "You Win!!!";
+      console.log(result_txt);
+      // document.getElementById(result_id).innerHTML = result_txt;
+  
+     } else if (decision == "blank") {
+      result_txt = " ";
+      console.log(result_txt);
+     } else if (decision == "lost") {
+     result_txt = "Try Again!";
+     console.log(result_txt);
+     }
+    }
+
+    document.getElementById(result_id).innerHTML = result_txt;
